b072f20f33f38ac7d06d458bda7268246864ebf9
Your Name <your.email@example.com>
2025-11-06 17:53:20 +0900

    251106_1753

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function App() {
-  return <div>안녕하세요</div>;
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sapiente,
+      error unde facere repellat eaque nemo commodi veritatis iure impedit atque
+      minus ad officia tenetur cum nam dolor adipisci numquam.
+    </div>
+  );
 }
 
 export default App;
