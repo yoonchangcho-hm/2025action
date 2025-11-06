d18a9aa7fde6260d85d3dd65a4bd7de800bc4281
Your Name <your.email@example.com>
2025-11-06 17:58:58 +0900

    251106_1758

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 function App() {
   return (
     <div>
+      <h3>Lorem, ipsum dolor.</h3>
+      <br />
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sapiente,
       error unde facere repellat eaque nemo commodi veritatis iure impedit atque
       minus ad officia tenetur cum nam dolor adipisci numquam.
